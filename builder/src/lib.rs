20d8915fcf560f9fb13abcbbc08efda036254570
inokawa <48897392+inokawa@users.noreply.github.com>
2021-03-12 10:26:07 +0900

    Impl builder 03

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,16 +1,71 @@
 use proc_macro::TokenStream;
-use quote::quote;
-use syn::{parse_macro_input, DeriveInput};
+use quote::{format_ident, quote};
+use syn::{parse_macro_input, Data, DeriveInput, Fields};
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
     let input = parse_macro_input!(input as DeriveInput);
     let name = &input.ident;
+    let builder_name = format_ident!("{}Builder", name);
+    let builder_fields: Vec<proc_macro2::TokenStream>;
+    let setter_methods: Vec<proc_macro2::TokenStream>;
+    let initial_values: Vec<proc_macro2::TokenStream>;
+    match input.data {
+        Data::Struct(st) => match st.fields {
+            Fields::Named(fields) => {
+                builder_fields = fields
+                    .named
+                    .iter()
+                    .map(|f| {
+                        let ident = &f.ident;
+                        let ty = &f.ty;
+                        quote! { #ident: Option<#ty> }
+                    })
+                    .collect();
+                setter_methods = fields
+                    .named
+                    .iter()
+                    .map(|f| {
+                        let ident = &f.ident;
+                        let ty = &f.ty;
+                        quote! {
+                            pub fn #ident(&mut self, #ident: #ty) -> &mut Self {
+                                self.#ident = Some(#ident);
+                                self
+                            }
+                        }
+                    })
+                    .collect();
+                initial_values = fields
+                    .named
+                    .iter()
+                    .map(|f| {
+                        let ident = &f.ident;
+                        // let ty = &f.ty;
+                        quote! { #ident: None }
+                    })
+                    .collect();
+            }
+            Fields::Unnamed(_) => unimplemented!(),
+            Fields::Unit => unimplemented!(),
+        },
+        _ => unimplemented!(),
+    };
 
     let expanded = quote! {
+        pub struct #builder_name {
+            #(#builder_fields,)*
+        }
+
+        impl #builder_name {
+            #(#setter_methods)*
+        }
+
         impl #name {
-            pub fn builder() -> bool {
-                true
+            pub fn builder() -> #builder_name {
+                #builder_name {
+                    #(#initial_values,)*
+                }
             }
         }
     };
